dcb3ef9f110c83d9368f1e3f5521e50dde697a95
Falar <fabio.lr81@gmail.com>
2024-03-06 08:57:34 +0100

    add vercel json and order data by date desc

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/status', (request, response) => {
 });
 
 app.get('/sampleData', (req, res) => {
-    ExpensesModel.find({}).sort({date:'asc' })
+    ExpensesModel.find({}).sort({date:'desc' })
         .then(expenses=>res.json(expenses))
         .catch(err => res.json(err));
     // res.json(data);
